531c3f4fc6e5ca48c6adabe7484a59a2e4a5c75b
Adame <Adame.haouari@gmail.com>
2020-12-28 14:26:51 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+let phrase = "J'aime les tartines et j'en mange"
+console.log(phrase)
+
+let nombre = "4 par jours"
+
+console.log(phrase + " " + nombre);
+
+let troisieme = 5;
+
+let quatrieme = 35;
+
+let addition = troisieme + quatrieme
+console.log(addition)
+
+let liee = phrase + troisieme
+console.log(liee)
+
+// fin exo 1
+
+let salut = "salut tout le monde"
+console.log(salut)
+
+let arv = "aurevoir !"
+console.log(arv)
+
+salut = arv
+console.log(salut)
+
+// fin exo 3
+
+
+let quatre = 4
+console.log(quatre)
+
+let six = "6"
+console.log(six)
+
+console.log(quatre != six)
+
+// quatre > six
+
+// quatre < six
+
+// quatre == six
+
+// quatre != six
+
+// quatre === six
+
+// quatre !==  six
